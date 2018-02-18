49ac20b845b484d5280c3887fc7243cf089ac51c
Michael Lee <943662850@qq.com>
2018-02-18 15:55:38 +0800

    2/1

--- js/spa.js
+++ js/spa.js
@@ -8,9 +8,7 @@
 /* global $,spa */
 var spa = (function(){
     var initModule = function ($container){
-        $container.html(
-        '<h1 style="display:inline-block; margin:25px;">Hello world</h1>'
-        );
+        spa.shell.initModule( $container );
     };
     return {initModule : initModule};
 }());
