64682a7a74af394f35f5ceb7e893828b7e4482af
Michael <943662850@qq.com>
2018-03-01 23:56:49 +0800

    3/1

--- js/spa.shell.js
+++ js/spa.shell.js
@@ -1,11 +1,15 @@
-/*shell module for SPA*/
+/*
+ * spa.shell.js
+ * Shell module for SPA
+*/
+
 /*jslint         browser : true, continue : true,
   devel  : true, indent  : 2,    maxerr   : 50,
   newcap : true, nomen   : true, plusplus : true,
   regexp : true, sloppy  : true, vars     : false,
   white  : true
 */
-/* global $,spa */
+/*global $, spa */
 
 spa.shell = (function(){
     // 'use strict';
